acfb19ba135a45c3b032c7108bb98e09db7bc5d5
Meti.A <blackpearl@localhost.localdomain>
2021-01-01 23:51:04 -0500

    Counting Valleys edited

--- week1/counting_valleys.py
+++ week1/counting_valleys.py
@@ -1,23 +1,23 @@
-#
-# def counter(steps,path):
-#     valid=True
-#     for i in path:
-#         if i not in 'UD':
-#             valid=False
-#             break
-#     if steps<2 or steps>10**6 or len(path)!=steps or valid==False:
-#         print("invalid input")
-#         return()
-#     slope=0
-#     valley=0
-#     for i in path:
-#         if i=='U':
-#             slope +=1
-#         elif i=='D':
-#             slope -=1
-#         if i=='U' and slope==0:
-#             valley +=1
-#     return(valley)
-# print(counter(8,input()))
+def counter(steps,path):
+    is_valid=True
+    for i in path:
+        if i not in 'UD':
+            is_valid=False
+            break
+    if steps < 2 or steps>10**6 or len(path) != steps or is_valid==False:
+        return 0
+
+    else:
+        slope = 0
+        valley = 0
+        for i in path:
+            if i == 'U':
+                slope += 1
+            elif i == 'D':
+                slope -= 1
+            if i == 'U' and slope == 0:
+                valley += 1
+        return(valley)
+
 
 
