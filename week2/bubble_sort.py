bf0dbef4b9b01a0f4d8dcc0694faca6862cf3918
Meti.A <blackpearl@localhost.localdomain>
2021-01-06 08:20:14 -0500

    solutions to week  challenges2

---
+++ week2/bubble_sort.py
@@ -0,0 +1,17 @@
+def swap(m, a, b):
+    c = m[a]
+    m[a] = m[b]
+    m[b] = c
+    return m
+def bubleSort(l1):
+    for i in range (len(l1)):
+        for j in range (len(l1)):
+            if l1[i] < l1[j]:
+                swap(l1, i, j)
+    return l1
+
+
+
+l1= [0, 21, 26, 17, 81, 2]
+bubleSort(l1)
+print(l1)
