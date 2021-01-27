3674874abde335d33c142e31e29cdad03a4c2e7f
Meti A <metiade7@gmail.com>
2021-01-27 17:22:14 -0500

    solution

---
+++ week4/crawel_logger.py
@@ -0,0 +1,14 @@
+#question url : "https://leetcode.com/problems/crawler-log-folder/"
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        operation = 0
+        for i in logs:
+            if i == "./":
+                continue
+            elif i == "../":
+                if operation > 0:
+                    operation -= 1
+            else:
+                operation += 1
+        return operation 
