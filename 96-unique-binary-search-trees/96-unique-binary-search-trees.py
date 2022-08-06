d425f701cef4c04fd25c73ba48a08edb4fca9880
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-08-06 07:14:09 +0300

    Time: 38 ms (77.20%), Space: 13.9 MB (59.24%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,19 +1,35 @@
 class Solution:
     def numTrees(self, m: int) -> int:
         
-        dp = dict()
-        dp[0] = 1
-        dp[1] = 1
+        
+        
+        def recursive():
+            dp = dict()
+            dp[0] = 1
+            dp[1] = 1
 
-        def calculate(n):
-            if n in dp:
+            def calculate(n):
+                if n in dp:
+                    return dp[n]
+                runningTotal = 0
+                for i in range(1, n+1):
+                    runningTotal += calculate(i-1) * calculate(n-i)
+                dp[n] = runningTotal
                 return dp[n]
-            runningTotal = 0
-            for i in range(1, n+1):
-                runningTotal += calculate(i-1) * calculate(n-i)
-            dp[n] = runningTotal
-            return dp[n]
 
-        return calculate(m)
-
-      
+            return calculate(m)
+        
+        def iterative():
+            dp = dict()
+            dp[0] = 1
+            dp[1] = 1
+            
+            for i in range(2, m+1):
+                runningTotal = 0
+                for j in range(1, i+1):
+                    runningTotal += dp[j-1] * dp[i-j]
+                dp[i] = runningTotal
+                    
+            return dp[m]
+        
+        return iterative()
