ce96c4f689360fb9952600f6559693347059a5cc
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-08-09 06:43:57 +0300

    Time: 826 ms (33.83%), Space: 16 MB (7.58%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.py
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.py
@@ -1,26 +1,22 @@
 class Solution:
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
-        counter = Counter(arr)
-        dp = [0] * len(arr)
+        dp = Counter(arr)
         self.ans = 0 
         arr.sort()
         
         def recurse(index):
             if index >= len(arr):
                 return 0
-            if dp[index] > 0 :
-                return dp[index]
-            dp[index] = 1
-            
+            parent = arr[index]
             for j in range(index):
-                parent, child1 = arr[index], arr[j]
-                if (parent / child1) in counter:
-                    child2index = arr.index(parent/child1)
-                    dp[index] += (recurse(j) * recurse(child2index))
+                child1 = arr[j]
+                child2 = parent /child1
+                if child2 in dp:
+                    dp[parent] += dp[child1] * dp[child2]
                     
             recurse(index+1)
-            self.ans += dp[index]
-            return dp[index]
+            self.ans += dp[parent]
+            return dp[parent]
         
         recurse(0)
         return self.ans% (10**9+7)
