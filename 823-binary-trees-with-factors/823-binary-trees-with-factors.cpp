21f67ae6cbd62ddbca9a35c9af7631e328ec58aa
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-08-09 07:43:40 +0300

    Time: 97 ms (66.15%), Space: 17.3 MB (5.85%) - LeetHub

---
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    long long int ans = 0;
+    unordered_map<long int, long long int> dp;
+    int mod = pow(10, 9) + 7;
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        
+        for (int i=0; i < arr.size(); i++){
+            dp[arr[i]] = 1;
+        }
+        
+        sort(arr.begin(), arr.end());
+        recurse(0, arr);
+        return ans % mod;
+        
+    }
+    
+    long long int recurse(int index, vector<int>& arr){
+            if (index >= arr.size()){
+                return 0;
+            }
+            
+            int parent = arr[index];
+            
+            for (int j=0; j < index; j++){
+                int child1 = arr[j], child2 = parent / double(child1);
+                if (parent % child1 != 0) continue;
+                
+                dp[parent] += (dp[child1] * dp[child2]);
+                
+            }
+            recurse(index+1, arr);
+            
+            ans += dp[parent];
+            return dp[parent];
+        }
+};
