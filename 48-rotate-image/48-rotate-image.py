7ddbc969e48cc8b8cdd2b0eb3b7205b97e4072cd
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-08-31 14:33:18 +0300

    Time: 36 ms (93.83%), Space: 14 MB (29.94%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -1,16 +1,40 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        self.transpose(matrix)
-        self.reflect(matrix)
-    
-    def transpose(self, matrix):
-        n = len(matrix)
-        for i in range(n):
-            for j in range(i + 1, n):
-                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
-
-    def reflect(self, matrix):
-        n = len(matrix)
-        for i in range(n):
-            for j in range(n // 2):
-                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
+        n, m = len(matrix), len(matrix[0])
+        left, right, top , bottom = 0, m-1, 0, n-1
+        
+        
+        while left < right and top < bottom :
+            arr = []
+            # update right most cells 
+            for i in range(left+1, right+1):
+                arr.append(matrix[top][i])
+            matrix[top][right] = matrix[top][left]
+            j = 0 
+            for i in range(top+1, bottom+1):
+                temp = matrix[i][right] 
+                matrix[i][right] = arr[j]
+                arr[j] = temp
+                j += 1
+            right -= 1
+            # update bottom cells
+            j = 0
+            for i in range(right, left-1, -1):
+                temp = matrix[bottom][i]
+                matrix[bottom][i] = arr[j]
+                arr[j] = temp
+                j += 1
+            bottom -= 1
+            j = 0
+            for i in range(bottom, top-1, -1):
+                temp = matrix[i][left]
+                matrix[i][left] = arr[j]
+                arr[j] = temp
+                j += 1
+            left += 1
+            j = 0 
+            for i in range(left, right+1):
+                temp = matrix[top][i]
+                matrix[top][i] = arr[j]
+                j += 1
+            top += 1
