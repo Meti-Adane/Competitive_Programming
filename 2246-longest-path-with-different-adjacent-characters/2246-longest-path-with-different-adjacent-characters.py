9ca986792492d9b78b63e6ac42ae8ba54ddad08b
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-09-07 16:49:28 +0300

    Time: 4075 ms (21.99%), Space: 153 MB (54.90%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -1,7 +1,6 @@
 class Solution:
     def longestPath(self, arr: List[int], s: str) -> int:
         graph = defaultdict(list)
-        visited = [False for _ in range(len(arr))]
         
         for i, parent in enumerate(arr):
             graph[parent].append(i)
@@ -9,15 +8,12 @@ class Solution:
         self.longest = 1 
         
         
-        def dfs(node, lastchar):
-            if s[node] == lastchar:
-                return 0
-            visited[node] = True
+        def dfs(node):
             max1, max2, count = 0, 0, 1
             for child in graph[node]:
-                ans = dfs(child, s[node]) 
-                count = max(ans+1, count)
-                if ans > 0:
+                ans = dfs(child) 
+                if s[child] != s[node]:
+                    count = max(ans+1, count)
                     if ans > max2:
                         max1, max2 = max2, ans
                     elif ans > max1:
@@ -26,7 +22,6 @@ class Solution:
             
             return count
         
-        for i in range(len(arr)):
-            if not visited[i]:
-                dfs(i, "")
+        
+        dfs(0)
         return self.longest
