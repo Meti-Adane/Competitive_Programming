514b2c6f6b7b8fcd8a0fd3b6455b707ded8a7fd2
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-09-25 15:03:00 +0300

    Time: 104 ms (65.54%), Space: 13.9 MB (45.58%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.py
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.py
@@ -5,13 +5,41 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        stack1 = []
-        stack2 = []
-        ans = None
         head = None
-        temp1 = l1
-        temp2 = l2
-        
+        temp1, temp2 = l1, l2
+        stack1, stack2 = self.fillStacks(temp1, temp2)
+                
+        carry = 0
+        while stack1 or stack2:
+            res = self.getSum(stack1, stack2) + carry
+            unitsplace, carry = res, 0
+            if res >= 10:
+                unitsplace, carry = res % 10, res // 10
+            if not head:
+                head = ListNode(unitsplace)
+            else:
+                newnode = ListNode(unitsplace, head)
+                head = newnode
+        if carry >= 1:
+            newnode = ListNode(carry, head)
+            head = newnode
+        return head
+    
+    
+    
+    def getSum(self, stack1, stack2):
+        if not stack1:
+            val1 = 0
+        else:
+            val1 = stack1.pop()
+        if not stack2:
+            val2 = 0
+        else:
+            val2 = stack2.pop()
+        return val1+val2
+    
+    def fillStacks(self, temp1, temp2):
+        stack1, stack2 = [], []
         while temp1 or temp2:
             if temp1:
                 stack1.append(temp1.val)
@@ -19,36 +47,4 @@ class Solution:
             if temp2:
                 stack2.append(temp2.val)
                 temp2 = temp2.next
-        carry = 0
-        while stack1 or stack2:
-            if not stack1:
-                val1 = 0
-            else:
-                val1 = stack1.pop()
-            if not stack2:
-                val2 = 0
-            else:
-                val2 = stack2.pop()
-                
-            res = val1 + val2 + carry
-            if res >= 10:
-                unitsplace = res % 10
-                carry = res // 10
-            else:
-                unitsplace = res
-                carry = 0
-            if not ans:
-                ans = ListNode(unitsplace)
-                head = ans
-            else:
-                newnode = ListNode(unitsplace)
-                temp = head
-                newnode.next = temp
-                head = newnode
-        if carry >= 1:
-            newnode = ListNode(carry)
-            temp = head
-            newnode.next = temp
-            head = newnode
-        return head
-            
+        return stack1, stack2
