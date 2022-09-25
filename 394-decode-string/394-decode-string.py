58e68d3accdf124eb74fee898a17f21c85635cba
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-09-25 22:47:59 +0300

    Time: 47 ms (58.51%), Space: 14.1 MB (20.25%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,7 +1,6 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack = []
-        nums = []
+        stack, nums, opening = [], [], []
         digits = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
         i = 0 
         
@@ -12,18 +11,16 @@ class Solution:
                 while i+1 < len(s) and s[i+1] in digits:
                     num += s[i+1]
                     i += 1
-                
                 nums.append(int(num))
             elif char == ']':
-                que = deque()
-                while stack and stack[-1] != '[':
-                    que.appendleft(stack.pop())
-                if stack and stack[-1] == '[':
-                    stack.pop()
-                if nums:
-                    mult = nums.pop()
-                newstr = mult * "".join(que)
+                lastopening = opening.pop()
+                prevstr = "".join(stack[lastopening:])
+                stack = stack[:lastopening]
+                mult = nums.pop()
+                newstr = mult * prevstr
                 stack.append(newstr)
+            elif char == '[':
+                opening.append(len(stack))
             else:
                 stack.append(char)
             i += 1
