140ab7c6e2e6c6124507e86dc9cbf73d49311802
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-10-03 03:59:14 +0300

    Time: 3285 ms (6.71%), Space: 25.1 MB (26.96%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -1,18 +1,14 @@
 class Solution:
     def minCost(self, s: str, neededTime: List[int]) -> int:
         ans = left = 0 
-        
         while left < len(s):
             maxEffort = totalEffort = neededTime[left]
-            isEffortRequired = False
             right = left + 1
             while right < len(neededTime) and s[right] == s[left]:
-                isEffortRequired = True
                 totalEffort += neededTime[right]
                 maxEffort = max(maxEffort, neededTime[right])
                 right += 1
-            if isEffortRequired:
-                ans += (totalEffort - maxEffort)
+            ans += (totalEffort - maxEffort)
             left = right
             
         return ans
