50c6906d14734798dabe5f4973bd677e73172d11
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-10-03 14:44:15 +0300

    Time: 587 ms (49.86%), Space: 17.7 MB (31.04%) - LeetHub

--- 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.py
+++ 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.py
@@ -3,14 +3,14 @@ class Solution:
         
         hashmap = {0:1} # hashmap to store the number of odd occurrence 
         count = 0 
-        oddOccurrence = 0 
+        runningSum = 0 
         
         for num in nums:
-            if num % 2 == 1:
-                oddOccurrence += 1
-            if oddOccurrence - goal in hashmap:
-                count += hashmap[oddOccurrence - goal] 
-            hashmap[oddOccurrence] = hashmap.get(oddOccurrence, 0) + 1
+            if num == 1:
+                runningSum += 1
+            if runningSum - goal in hashmap:
+                count += hashmap[runningSum - goal] 
+            hashmap[runningSum] = hashmap.get(runningSum, 0) + 1
         return count 
                 
             
