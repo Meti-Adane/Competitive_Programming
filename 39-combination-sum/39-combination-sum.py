0ba85d9b8c94a3553a8898c65508c68835a1ada8
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-10-08 21:08:01 +0300

    Time: 141 ms (49.76%), Space: 14 MB (82.76%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,17 +1,38 @@
 class Solution:
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
-        def combine(index, runningSum, stack, combinations):
-            if runningSum == target:
-                combinations.append(stack.copy())
+        
+        def recursive():
+            def combine(index, runningSum, stack, combinations):
+                if runningSum == target:
+                    combinations.append(stack.copy())
+                    return combinations
+                if runningSum > target or index >= len(nums):
+                    return combinations
+
+
+                stack.append(nums[index])
+                combinations = combine(index, runningSum+nums[index], stack, combinations)
+                stack.pop()
+                combinations = combine(index+1, runningSum, stack, combinations)
                 return combinations
-            if runningSum > target or index >= len(nums):
-                return combinations
+            return combine(0, 0, [], [])
+        
+        def iterative():
+            stack = []
+            ans = []
+            stack.append((0, 0, []))
 
-
-            stack.append(nums[index])
-            combinations = combine(index, runningSum+nums[index], stack, combinations)
-            stack.pop()
-            combinations = combine(index+1, runningSum, stack, combinations)
-            return combinations
-
-        return combine(0, 0, [], [])
+            while stack:
+                index, runningSum, path = stack.pop()
+                if index >= len(nums) or runningSum > target:
+                    continue
+                if runningSum == target:
+                    ans.append(path.copy())
+                else:
+                    newans = path.copy()
+                    stack.append((index, runningSum+nums[index], path.copy() + [nums[index]]))
+                    stack.append((index+1, runningSum, path.copy()))
+            return ans
+        
+        #return iterative()
+        return recursive()
