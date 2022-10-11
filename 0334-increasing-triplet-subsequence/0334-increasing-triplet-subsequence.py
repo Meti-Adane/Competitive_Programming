b004d7422605fb2dcb9d172136f7bf544a27bfb2
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-10-11 10:10:03 +0300

    Time: 1451 ms (26.64%), Space: 24.6 MB (48.78%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -3,7 +3,7 @@ class Solution:
         firstsmaller = secondsmaller = float('inf')
         
         for num in nums:
-            if num > secondsmaller: 
+            if num > secondsmaller: #checks if the previous sequence was a valid triplet [5, 7, 2, 9] --> when we get to 2 first smaller gets updated so when we get to nine by placing this check first ene i get to see if there was a previous pair (here preserved by second smaller number) that could be combined with this new num ena give me valid triplet
                 return True
             if num <= firstsmaller:
                 firstsmaller = num
