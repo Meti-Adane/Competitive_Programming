a8c0eaf1d2f669e74dca5f87e877e4c452684b30
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-10-17 01:10:23 +0300

    Time: 40 ms (84.84%), Space: 13.9 MB (16.43%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,7 +1,7 @@
 class Solution:
     def reverse(self, x: int) -> int:
         ans, sign = 0, 1 
-        lbound, ubound = (-2**31), (2**31)-1
+        lbound, ubound = -2147483648, 2147483647
         if x < 0:
             sign = -1
             x *= -1
