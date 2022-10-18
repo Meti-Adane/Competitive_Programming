626716c772bfea15229d9300d94ddfc67fbb6d7a
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-10-18 19:17:33 +0300

    Time: 625 ms (76.46%), Space: 21 MB (8.05%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        n, m = len(matrix), len(matrix[0])
+        memo = dict()
+        isValid = lambda r,c,v : 0 <= r < n and 0 <= c < m and (r,c) not in v 
+        dirs = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        
+        def dfs(row, col, visited):
+            if (row, col) in memo: 
+                return memo[(row, col)] 
+            visited.add((row, col))
+            maxlength = 1
+            for x, y in dirs:
+                if isValid(x+row, col+y, visited) and matrix[row][col] < matrix[row+x][col+y]:
+                    maxlength = max(dfs(row+x, col+y, visited)+1, maxlength)
+            memo[(row,col)] = maxlength 
+            visited.remove((row, col))
+            return memo[(row, col)] 
+        
+        ans = 0       
+        for i in range(n):
+            for j in range(m):
+                ans = max(ans, dfs(i, j, set()))
+        return ans
