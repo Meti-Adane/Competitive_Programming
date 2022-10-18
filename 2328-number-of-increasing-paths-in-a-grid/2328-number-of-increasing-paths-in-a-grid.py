4b9af1fa444d36d3ea71302b30728a5f4ca3a1b3
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-10-18 20:13:19 +0300

    Time: 6928 ms (19.20%), Space: 152.3 MB (13.34%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
@@ -1,27 +1,27 @@
 class Solution:
     def countPaths(self, grid: List[List[int]]) -> int:
         memo, n, m = dict(), len(grid), len(grid[0])
-        isValid = lambda r,c,v : (0 <= r < n and 0 <= c < m and (r,c) not in v)
-        count, mod = 0, pow(10, 9) + 7 
+        mod = pow(10, 9) + 7 
+        dirs = [(0, 1), (0, -1), (-1, 0), (1, 0)]
         
-        def dfs(r, c, visited):
+        isValid = lambda r,c: (0 <= r < n and 0 <= c < m)
+        
+        def dfs(r, c):
             if (r,c) in memo:
                 return memo[(r,c)]
-            visited.add((r,c))
+            
             path = 1
-            for x, y in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
-                if isValid(r+x, c+y, visited) and grid[r+x][c+y] > grid[r][c]:
-                    path += dfs(r+x, c+y, visited)
-            visited.remove((r, c))
+            for x, y in dirs:
+                if isValid(r+x, c+y) and grid[r+x][c+y] > grid[r][c]:
+                    path += dfs(r+x, c+y) % mod
+                    
             memo[(r,c)] = path
-            return path
+            return memo[(r,c)]
         
+        count = 0
         for i in range(n):
             for j in range(m):
-                if (i, j) in memo:
-                    count += memo[(i,j)]
-                else:
-                    count += dfs(i, j, set())
+                count += dfs(i, j) 
        
         return count % mod
             
