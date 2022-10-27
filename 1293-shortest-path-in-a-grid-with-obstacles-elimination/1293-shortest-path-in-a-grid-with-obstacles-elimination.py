00a900d64484afb3e9e4b0a1b156c96b0c178ebc
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-10-27 17:03:27 +0300

    Time: 3268 ms (5.03%), Space: 21.5 MB (33.57%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -0,0 +1,30 @@
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        que = deque([(0, 0, 0, k)]) 
+        visited = set() 
+        n, m = len(grid), len(grid[0])
+        isvalid = lambda x, y, v, k: 0 <= x < n and 0 <= y < m and (x,y, k) not in v 
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        while que:
+            step, row, col, k = que.popleft()
+            if row == n-1 and col == m-1:
+                return step
+            
+            for x, y in dirs:
+                newrow, newcol = row+x, col+y
+                if newrow == n-1 and newcol == m-1:
+                    return step +1
+                    
+                if k and isvalid(newrow, newcol, visited, k-1) and grid[newrow][newcol] == 1:
+                    self.addToPath(step+1, newrow, newcol, k-1, que, visited)
+                
+                elif isvalid(newrow, newcol, visited, k) and grid[newrow][newcol] == 0:
+                    
+                    self.addToPath(step+1, newrow, newcol, k, que, visited)
+        return -1
+                
+                
+    def addToPath(self, step, row, col, k, que, visited):
+            que.append((step, row, col, k))
+            visited.add((row, col, k)) 
