2d10333d660bff277b1198135e4ae33774fdb110
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-10-31 08:51:13 +0300

    Time: 212 ms (25.21%), Space: 13.9 MB (78.65%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,10 +1,9 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         n, m = len(matrix), len(matrix[0])
-        isValid = lambda x,y: (x<n and y<m)
         
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if isValid(i+1, j+1) and matrix[i][j] != matrix[i+1][j+1]:
+                if i+1 < n and j+1 < m and matrix[i][j] != matrix[i+1][j+1]:
                     return False
         return True 
