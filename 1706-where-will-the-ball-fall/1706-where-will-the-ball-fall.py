546c7224d19e9946cafa629982ce28388e41d201
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-11-02 05:59:42 +0300

    Time: 287 ms (76.63%), Space: 15.5 MB (5.57%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -3,18 +3,24 @@ class Solution:
         n, m = len(grid), len(grid[0])
         canPass = lambda x, y, k:  0 <= k < m and grid[x][y] == grid[x][k]
             
-        def outCol(row, col):
+        def outCol(row, col, visited):
+            if (row,col) in visited:
+                return visited[(row, col)]
             if row == n :
                 return col
             
             newcol = col + grid[row][col]
+            visited[(row, col)] = -1
             if canPass(row, col, newcol):
-                return outCol(row+1, newcol)
-            return -1
+                visited[(row, col)] = outCol(row+1, newcol, visited)
                 
+            return visited[(row, col)]
+        
+        dp = dict()
         ans = []
+        
         for i in range(m):
-            ans.append(outCol(0, i))
+            ans.append(outCol(0, i, dp))
         return ans
     
             
