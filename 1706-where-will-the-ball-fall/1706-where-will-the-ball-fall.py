2f840e956b7e7db0530e9990d505f972fb74db73
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-11-02 05:55:15 +0300

    Time: 664 ms (5.81%), Space: 15.5 MB (5.57%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findBall(self, grid: List[List[int]]) -> List[int]:
+        n, m = len(grid), len(grid[0])
+        canPass = lambda x, y, k:  0 <= k < m and grid[x][y] == grid[x][k]
+            
+        def outCol(row, col, visited):
+            if (row,col) in visited:
+                return visited[(row, col)]
+            if row == n :
+                return col
+            
+            newcol = col + grid[row][col]
+            if canPass(row, col, newcol):
+                visited[(row, col)] = outCol(row+1, newcol, visited)
+            else:
+                visited[(row, col)] = -1
+            return visited[(row, col)]
+        
+        dp = dict()
+        ans = []
+        
+        for i in range(m):
+            ans.append(outCol(0, i, dp))
+        return ans
+    
+            
