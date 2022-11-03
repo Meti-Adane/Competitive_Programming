e7c492f756298896724f6e3a94bc07b794690be0
Meti Adane B <53035177+Meti-Adane@users.noreply.github.com>
2022-11-03 15:08:10 +0300

    Time: 2931 ms (55.43%), Space: 37.9 MB (96.80%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,25 +1,14 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
-        seen, palindromes, count = dict(), [], 0
-        hasPivotPalindrome = lambda arr, hashmap : any([ hashmap[w] % 2 == 1 for w in arr ])
+        seen, count, pivot = Counter(words), 0, False
         
-        for word in words:
-            if word[0] == word[1]:
-                palindromes.append(word)
-            seen[word] = seen.get(word, 0) + 1
-            
-        count += 2 if hasPivotPalindrome(palindromes, seen) else 0
-
         for word, freq in seen.items():
             reverse = word[::-1]
-            x = y = 0
             if reverse == word:
-                y, x = 2, seen[word] if seen[word] % 2 == 0 else seen[word] - 1
+                pivot |= (freq % 2 == 1)
+                count += (2 * (freq if freq % 2 == 0 else freq-1))
             elif reverse in seen:
-                y, x =4, min(seen[word], seen[reverse])
-                seen[reverse] = 0 
-            
-            count += (x*y)
+                count += (4 * min(freq, seen[reverse]))
             seen[word] = 0
-           
-        return count  
+        
+        return count + 2 if pivot else count
