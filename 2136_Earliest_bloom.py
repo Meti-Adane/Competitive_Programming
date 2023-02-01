d283c2118fc1a3d271d03d02359260a06b1e5db1
Meti A. Bayissa <meti@Metis-MacBook-Pro.local>
2023-02-01 22:05:35 +0300

    leetcode

--- 2136_Earliest_bloom.py
+++ 2136_Earliest_bloom.py
@@ -1,7 +1,11 @@
 class Solution:
     def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
-        ans = currPlantTime = 0 
-        for growth, plant in sorted(zip(growTime, plantTime), reverse=True):
+        ans = 0
+        currPlantTime = 0 
+        arr = list(zip(growTime, plantTime))
+        arr.sort(reverse=True)
+        
+        for growth, plant in arr:
             currPlantTime += plant
             ans = max(ans, growth+currPlantTime)
         return ans 
