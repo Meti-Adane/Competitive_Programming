9390b5101cf81411b96f5c49bee1050bf29c8029
Meti A. Bayissa <meti@Metis-MacBook-Pro.local>
2023-02-20 16:27:15 +0300

    round 4 day 1

--- 809. Expressive Words.py
+++ 809. Expressive Words.py
@@ -1,16 +1,17 @@
 class Solution:
     def expressiveWords(self, s: str, words: List[str]) -> int:
-        return sum(self.is_strechy(s, word) for word in words)
-
-    def is_strechy(self, s: str, word: str) -> bool:
-        i = j = 0
-        while i < len(s) and j < len(word) and s[i] == word[j]:
-            count_s = count_word = 1
-            while i + count_s < len(s) and s[i] == s[i+count_s]:
-                    count_s+=1
-            while j + count_word < len(word) and word[j] == word[j+count_word]:
-                    count_word +=1
-            i, j = i+count_s, j+count_word
-            if count_word < count_s < 3 or count_word > count_s: 
-                return False				
-        return i == len(s) and j == len(word)
+        
+        def streches(s, word) :
+            i = j = 0
+            while i < len(s) and j < len(word) and s[i] == word[j]:
+                count_s = count_word = 1
+                while i + count_s < len(s) and s[i] == s[i+count_s]:
+                        count_s+=1
+                while j + count_word < len(word) and word[j] == word[j+count_word]:
+                        count_word +=1
+                i, j = i+count_s, j+count_word
+                if count_word < count_s < 3 or count_word > count_s: 
+                    return False				
+            return i == len(s) and j == len(word)
+        
+        return sum(streches(s, word) for word in words)
