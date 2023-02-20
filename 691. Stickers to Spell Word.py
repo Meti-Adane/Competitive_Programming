e9d2e56691a04aec1dfbc7e207dd041c690cfeec
Meti A. Bayissa <meti@Metis-MacBook-Pro.local>
2023-02-20 16:50:40 +0300

    round 4 day 1

--- 691. Stickers to Spell Word.py
+++ 691. Stickers to Spell Word.py
@@ -1,20 +1,20 @@
 class Solution:
-    def expressiveWords(self, s: str, words: List[str]) -> int:
-        
-        ans = 0 
-        def helper(word) :
-            i = j = 0
-            while i < len(s) and j < len(word) and s[i] == word[j]:
-                count_s = count_word = 1
-                while i + count_s < len(s) and s[i] == s[i+count_s]:
-                        count_s+=1
-                while j + count_word < len(word) and word[j] == word[j+count_word]:
-                        count_word +=1
-                i, j = i+count_s, j+count_word
-                if count_word < count_s < 3 or count_word > count_s: 
-                    return 0				
-            return 1 if i == len(s) and j == len(word) else 0
-        
-        for word in words:
-             ans += helper(word)
-        return ans
+    def minStickers(self, stickers: List[str], target: str) -> int:
+        n = pow(2, len(target))
+        dp = [-1] * n
+        dp[0] = 0
+        for i in range(n):
+            if dp[i] == -1:
+                continue
+            for sticker in stickers:
+                curr = i
+                for char in sticker:
+                    for j in range(len(target)):
+                        if (curr >> j) & 1:
+                            continue
+                        if target[j] == char:
+                            curr |= 1 << j
+                            break
+                if dp[curr] == -1 or dp[curr] > dp[i] + 1:
+                    dp[curr] = dp[i] + 1
+        return dp[-1]
