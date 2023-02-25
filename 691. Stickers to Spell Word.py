d3dcda17ad2ceb91744c534c09309dbdbf439a21
Meti A. Bayissa <meti@Metis-MacBook-Pro.local>
2023-02-25 14:07:59 +0300

    round 4 day 1

--- 691. Stickers to Spell Word.py
+++ 691. Stickers to Spell Word.py
@@ -1,18 +1,20 @@
 class Solution:
     def minStickers(self, stickers: List[str], target: str) -> int:
         n = len(target)
-        dp = [-1] * (1 << n)
+        dp = [-1] * pow(2, n)
         dp[0] = 0
-        for state in range(1 << n):
-            if dp[state] == -1: continue
+        for state in range(pow(2, n)):
+            if dp[state] == -1:
+                continue
             for sticker in stickers:
-                now = state
+                new_state = state
                 for letter in sticker:
                     for i in range(n):
-                        if (now >> i) & 1: continue
+                        if (new_state >> i) & 1:
+                            continue
                         if target[i] == letter:
-                            now |= 1 << i
+                            new_state |= 1 << i
                             break
-                if dp[now] == -1 or dp[now] > dp[state] + 1:
-                    dp[now] = dp[state] + 1
+                if dp[new_state] == -1 or dp[new_state] > dp[state] + 1:
+                    dp[new_state] = dp[state] + 1
         return dp[-1]
