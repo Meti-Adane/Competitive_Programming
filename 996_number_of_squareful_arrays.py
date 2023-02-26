4dfcb47d7ebb64692118c310197f550812995db7
Meti A. Bayissa <meti@Metis-MacBook-Pro.local>
2023-02-26 08:53:00 +0300

    squid

--- 996_number_of_squareful_arrays.py
+++ 996_number_of_squareful_arrays.py
@@ -3,19 +3,17 @@ class Solution:
         
         ans = []
         nums.sort()
-        self.dfs(nums, [], ans)
-        return len(ans)
-    
-    def dfs(self, nums, path, ans):
-        if not nums:
-            ans.append(path)
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue 
-            if path and not self.square(path[-1]+nums[i]):
-                continue 
-            self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], ans)
         
-    def square(self, x):
-        from math import sqrt
-        return pow(int(sqrt(x)), 2) == x
+        def dfs(nums, path, ans):
+            if not nums:
+                ans.append(path)
+            for i in range(len(nums)):
+                if i > 0 and nums[i] == nums[i-1]:
+                    continue 
+                if path and not pow(sqrt(int(path[-1]+nums[i])), 2) == int(path[-1]+nums[i]):
+                    continue 
+                dfs(nums[:i]+nums[i+1:], path+[nums[i]], ans)
+            
+            return len(ans)
+        
+        return dfs(nums, [], ans)
