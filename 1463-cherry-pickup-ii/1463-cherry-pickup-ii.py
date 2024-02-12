c220abc796c5693daccae7e4f0ecad14da0a0df5
Meti Bayissa <53035177+Meti-Adane@users.noreply.github.com>
2024-02-12 16:21:36 +0300

    Time: 1228 ms (41.12%), Space: 60.1 MB (7.11%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -10,16 +10,12 @@ class Solution:
         def recurse(r1, c1, r2, c2):
             if (not isValid(r1, c1) or not isValid(r2, c2) 
                  or (r1, c1) == (r2, c2)): return 0 
+
             cherries = 0
-            
-            for x,y in directions:
-                newr1, newc1 = r1+x, c1+y
-                visited.add((newr1, newc1))
+            for x1,y1 in directions:
                 for x2, y2 in directions:
-                   newr2, newc2 = r2+x2, c2+y2
-                   if (newr2, newc2) not in visited:
-                       cherries = max(cherries, recurse(newr1, newc1, newr2, newc2))
-                visited.remove((newr1, newc1))
+                   if (r1+x1, c1+y1) != (r2+x2, c2+x2):
+                       cherries = max(cherries, recurse(r1+x1, c1+y1, r2+x2, c2+y2))
 
             cherries += (grid[r1][c1] + grid[r2][c2])
             return cherries  
